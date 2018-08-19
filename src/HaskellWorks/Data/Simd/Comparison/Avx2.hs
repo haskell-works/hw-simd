775f6c6b2c09096636d5a558a3876881045c444b
John Ky <newhoggy@gmail.com>
2018-08-19 18:33:32 +1000

    Fix AVX2 cmpeq8 alignment bug

--- src/HaskellWorks/Data/Simd/Comparison/Avx2.hs
+++ src/HaskellWorks/Data/Simd/Comparison/Avx2.hs
@@ -14,6 +14,7 @@ import qualified Foreign.ForeignPtr                      as F
 import qualified Foreign.Marshal.Unsafe                  as F
 import qualified Foreign.Ptr                             as F
 import qualified HaskellWorks.Data.ByteString            as BS
+import qualified HaskellWorks.Data.Simd.ChunkString      as CS
 import qualified HaskellWorks.Data.Simd.Internal.Foreign as F
 import qualified HaskellWorks.Data.Vector.AsVector8      as V
 import qualified HaskellWorks.Data.Vector.Storable       as DVS
@@ -35,7 +36,7 @@ instance CmpEqWord8s (DVS.Vector Word8) where
           let ending = DVS.padded ((DVS.length v + 63) `div` 64)  (DVS.drop alignment v)
           let (endFptr, _, _) = DVS.unsafeToForeignPtr ending
           F.withForeignPtr endFptr $ \endPtr -> do
-            void $ F.avx2Cmpeq8 (fromIntegral w8) (tgtPtr `F.plusPtr` (w64sLen * 64)) (fromIntegral w64sLen) (F.castPtr endPtr)
+            void $ F.avx2Cmpeq8 (fromIntegral w8) (tgtPtr `F.plusPtr` (w64sLen * 8)) (fromIntegral disalignment) (F.castPtr endPtr)
 
         return $ DVS.unsafeFromForeignPtr tgtFptr 0 tgtLen
     where (srcFptr, srcOffset, srcLen) = DVS.unsafeToForeignPtr v
@@ -81,7 +82,9 @@ instance CmpEqWord8s [BS.ByteString] where
   cmpEqWord8s w8 vs = BS.toByteString . cmpEqWord8s w8 . V.asVector8 <$> vs
   {-# INLINE cmpEqWord8s #-}
 
--- instance CmpEqWord8s CS.ChunkString where
---   type Target CS.ChunkString = CS.ChunkString
---   cmpEqWord8s w8 = CS.toChunkString . cmpEqWord8s w8 . BS.toByteStrings
---   {-# INLINE cmpEqWord8s #-}
+instance CmpEqWord8s CS.ChunkString where
+  type Target CS.ChunkString = CS.ChunkString
+  cmpEqWord8s w8 = CS.toChunkString . cmpEqWord8s w8 . BS.toByteStrings
+  {-# INLINE cmpEqWord8s #-}
+
+
